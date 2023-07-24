344b43e21ab95c863918587a64febd315cbb1da7
goomon <jaegoo.jho@gmail.com>
2023-07-24 20:26:18 +0900

    Feat: Input template

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const projInput = new ProjectInput();
