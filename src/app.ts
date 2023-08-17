8f7aff3e5eb7678733e0182ef487b3bb09ddce56
goomon <jaegoo.jho@gmail.com>
2023-08-17 11:16:18 +0900

    Refactor: rendering project with class

--- src/app.ts
+++ src/app.ts
@@ -142,6 +142,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList class
 class ProjectList extends  Component<HTMLDivElement, HTMLElement>{
     assignedProjects: Project[] = [];
@@ -168,10 +190,8 @@ class ProjectList extends  Component<HTMLDivElement, HTMLElement>{
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';
-        for (const projectItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = projectItem.title;
-            listEl.appendChild(listItem);
+        for (const project of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, project);
         }
     }
 }
